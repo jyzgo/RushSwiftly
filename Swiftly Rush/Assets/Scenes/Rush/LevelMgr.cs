53eed2a193a7d51f4fa8e1b1cb3898105bd238d2
jyzHome <jyzgo@163.com>
2018-05-18 22:58:10 +1000

    add Odin Inspector and touch event

--- Swiftly Rush/Assets/Scenes/Rush/LevelMgr.cs
+++ Swiftly Rush/Assets/Scenes/Rush/LevelMgr.cs
@@ -1,9 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System;
+using MonsterLove.StateMachine;
 
-public class LevelMgr : MonoBehaviour {
+enum PlayState
+{
+    Ready,
+    Playing,
+    Jumping,
+    Turning,
+    Dying
+};
+public class LevelMgr :MonoBehaviour
+{
+    
 
+    public static LevelMgr Current;
+
+    StateMachine<PlayState> _fsm;
+    
+    public void Init()
+    {
+        _fsm = StateMachine<PlayState>.Initialize(this, PlayState.Ready);
+    }
+
+    void Awke()
+    {
+        Current = this;
+        Init();
+    }
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +41,35 @@ public class LevelMgr : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void Ready_Enter()
+    {
+        Debug.Log("Ready");
+        _fsm.ChangeState(PlayState.Playing);
+    }
+
+    void Playing_Enter()
+    {
+        Debug.Log("Playing");
+    }
+
+    public void Touch()
+    {
+        Debug.Log("Touch");
+    }
+
+    public void SlideLeft()
+    {
+        Debug.Log("Left");
+    }
+
+    public void SlideRight()
+    {
+        Debug.Log("Right");
+    }
+
+    
+
+
 }
+
---
+++ Swiftly Rush/Assets/Scripts/UI/TouchControl.cs
@@ -0,0 +1,111 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class TouchControl : MonoBehaviour {
+
+    public void OnMouseUp()
+    {
+        if(Time.time > touchTime + TOUCH_MAX_TIME)
+        {
+            return;
+        }
+
+        fingerUp = Input.mousePosition;
+
+        if (verticalMove() > SWIPE_THRESHOLD && verticalMove() > horizontalValMove())
+        {
+            //Debug.Log("Vertical");
+            if (fingerDown.y - fingerUp.y > 0)//up swipe
+            {
+                OnSwipeDown();
+            }
+            else if (fingerDown.y - fingerUp.y < 0)//Down swipe
+            {
+                OnSwipeUp();
+            }
+            fingerUp = fingerDown;
+        }
+
+        //Check if Horizontal swipe
+        else if (horizontalValMove() > SWIPE_THRESHOLD && horizontalValMove() > verticalMove())
+        {
+            //Debug.Log("Horizontal");
+            if (fingerDown.x - fingerUp.x > 0)//Right swipe
+            {
+                OnSwipeLeft();
+            }
+            else if (fingerDown.x - fingerUp.x < 0)//Left swipe
+            {
+                OnSwipeRight();
+            }
+            fingerUp = fingerDown;
+        }
+
+        //No Movement at-all
+        else
+        {
+            //Debug.Log("No Swipe!");
+            OnClick();
+        }
+
+
+    }
+
+    private void OnSwipeLeft()
+    {
+        Debug.Log("Swipe left");
+    }
+
+    private void OnSwipeRight()
+    {
+        Debug.Log("Swipe right");
+    }
+
+    private void OnSwipeDown()
+    {
+        Debug.Log("Swipe down");
+    }
+
+    private void OnSwipeUp()
+    {
+        Debug.Log("Swipe up");
+    }
+
+    void OnClick()
+    {
+        Debug.Log("Click");
+    }
+
+    Vector3 fingerDown;
+    Vector3 fingerUp;
+    float touchTime;
+    const float TOUCH_MAX_TIME = 0.2f;
+    public void OnMouseDown()
+    {
+        touchTime = Time.time;
+
+        fingerDown = Input.mousePosition;
+    }
+
+    public float SWIPE_THRESHOLD = 20f;
+
+    public UnityEvent clickEvent;
+    public UnityEvent swipeLeft;
+    public UnityEvent swipeRight;
+
+    float verticalMove()
+    {
+        return Mathf.Abs(fingerUp.y - fingerDown.y);
+    }
+
+    float horizontalValMove()
+    {
+        return Mathf.Abs(fingerUp.x - fingerDown.x);
+    }
+}
+
+
