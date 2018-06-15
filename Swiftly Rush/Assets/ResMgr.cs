bee4ab88e78656e66b2a40cb1fc028c37858073c
jyzHome <jyzgo@163.com>
2018-06-15 19:24:54 +1000

    add block

---
+++ Swiftly Rush/Assets/ResMgr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResMgr : MonoBehaviour {
+
+    public GameObject Cube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Swiftly Rush/Assets/Scripts/Block.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    ResMgr _resMgr;
+    const float SIZE = 0.5f;
+    private void Awake()
+    {
+        _resMgr = FindObjectOfType<ResMgr>();
+    }
+    // Use this for initialization
+    void Start () {
+        
+		for(int i = 0; i <10;i++)
+        {
+            for (int j =0; j<5;j++)
+            {
+                var gb = Instantiate<GameObject>(_resMgr.Cube);
+                gb.transform.parent = transform;
+                gb.transform.position = new Vector3(j * SIZE,-SIZE,i * SIZE);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
